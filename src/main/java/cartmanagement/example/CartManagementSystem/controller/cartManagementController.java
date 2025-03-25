1fff03fb4338585c507d05d711e57c2ac20e3857
Hannah Edumoh <edumohemmaidiong@gmail.com>
2025-03-25 12:22:22 -0700

    cart system 2

--- src/main/java/cartmanagement/example/CartManagementSystem/controller/cartManagementController.java
+++ src/main/java/cartmanagement/example/CartManagementSystem/controller/cartManagementController.java
@@ -1,11 +1,8 @@
 package cartmanagement.example.CartManagementSystem.controller;
-
 import cartmanagement.example.CartManagementSystem.model.Cart;
 import cartmanagement.example.CartManagementSystem.model.CartItemRequest;
 import cartmanagement.example.CartManagementSystem.model.Response;
 import cartmanagement.example.CartManagementSystem.service.CartManagementService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,45 +15,57 @@ public class cartManagementController {
     public cartManagementController(CartManagementService cartManagementService) {
         this.cartManagementService = cartManagementService;
     }
+
+
     @PostMapping("/create-cart")
     public String createCart(@RequestBody Cart cart){
         cartManagementService.createACart(cart);
         return "Cart Created successfully";
     }
+
+
     @GetMapping("/get-all-carts")
     public List<Cart> getCarts(){
         return cartManagementService.getAllCarts();
     }
-    @PostMapping
-    public String addItemToCart(@RequestBody CartItemRequest cartItemRequest){
-        cartManagementService.addItem(cartItemRequest);
-        return "Item added to cart successfully";
-    }
-
 
     @GetMapping("{itemId}")
     public CartItemRequest getCartItem(@PathVariable("itemId") Long itemId){
         return cartManagementService.getItem(itemId);
     }
+
+
     @GetMapping()
     public Response getAllCartItem(){
         List<CartItemRequest> listResponse =  cartManagementService.getAllItems();
         Response finalResponse = new Response("00", "Successful", listResponse);
         return finalResponse;
     }
+
+    @PostMapping
+    public Response addItemToCart(@RequestBody CartItemRequest cartItemRequest){
+        String cartResponse = cartManagementService.addItem(cartItemRequest);
+        return new Response(cartResponse, "Item added to cart successfully",cartItemRequest);
+    }
+
+
     @PutMapping
-    public String updateCartItem(@RequestBody CartItemRequest cartItemRequest){
-        cartManagementService.updateItem(cartItemRequest);
-        return "Cart Item Updated successfully";
+    public Response updateCartItem(@RequestBody CartItemRequest cartItemRequest){
+        String cartItemRequest1 = cartManagementService.updateItem(cartItemRequest);
+        return new Response(cartItemRequest1, "Item updated successfully",cartItemRequest);
     }
+
+
     @DeleteMapping("{itemId}")
-    public String deleteCartItem(@PathVariable Long itemId){
-        cartManagementService.deleteItem(itemId);
-        return "Cart Item Deleted successfully";
+    public Response deleteCartItem(@PathVariable Long itemId){
+       String cartResponse =  cartManagementService.deleteItem(itemId);
+        return new Response(cartResponse, "Item deleted successfully", null);
     }
+
+
     @DeleteMapping("/api/cart/checkout-item")
-    public String checkoutCart(){
-      cartManagementService.checkoutCart();
-        return "Cart Cleared successfully";
+    public Response checkoutCart(){
+     String cartResponse = cartManagementService.checkoutCart();
+        return new Response(cartResponse, "Item checked out successfully", null);
     }
 }
